aacf9b1e3196baca755e71d30d67d4d54a96023d
Arvind_Suresh <53540697+Arvind0515@users.noreply.github.com>
2021-07-24 01:42:08 +0530

    changes

--- http_m.js
+++ http_m.js
@@ -1,8 +1,24 @@
 var http = require('http');
+var url = require('url');
 
 var httpserver = http.createServer(function (request,response)
 {
-    console.log("there is an request");
-    response.write("a request");
+    var pathname = url.parse(request.url).pathname;
+    switch(pathname)
+    {
+        case '/' :
+            console.log("there is an request");
+            response.write("a request");
+            break;
+        case '/hii':
+            console.log('hii');
+            response.write("hello");
+            break;
+        default:
+            console.log(pathname);
+            console.log("default");
+            break;
+    }
+    
 });
 httpserver.listen(8001);
